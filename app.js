41568b69949513f82e775049df78132b3cc67e64
Tia Piol <tiarox02@yahoo.com>
2025-08-06 08:34:57 -0700

    exercise 2

--- app.js
+++ app.js
@@ -18,3 +18,38 @@ const numsTimesTwo = nums.map((num) => {
 });
 
 console.log(numsTimesTwo);
+
+// ! Exercise 2:
+// a. Given the provided `pizzaToppings` array, use destructuring to extract 
+//    the first and second values and place them into variables. Name the 
+//    variable that corresponds to the first value `firstIngredient`. Name the 
+//    variable that corresponds to the second value `secondIngredient`.
+//
+// b. Console log the `firstIngredient` and `secondIngredient` variables.
+//
+// Starting code (don't modify this):
+
+const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+
+// Your code here:
+
+const [firstIngredient, secondIngredient] = pizzaToppings;
+
+console.log(firstIngredient);
+console.log(secondIngredient);
+
+// ! Exercise 3:
+// a. Given the provided `car` object, use destructuring to create two
+//    variables: `make` and `model` that will hold the respective values.
+//
+// b. Console log the `make` and `model` variables.
+//
+// Starting code (don't modify this):
+
+const car = {
+  make: 'Audi',
+  model: 'Q5',
+};
+
+// Your code here:
+
